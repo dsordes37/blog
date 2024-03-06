6726d0ed10ca77943ef671ce3e7b8b647ccba4ae
D'Sordes <125225427+dsordes37@users.noreply.github.com>
2024-03-06 01:50:36 -0300

    1.3.5

---
+++ angular/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    topNewsUrl: 'https://newsapi.org/v2/top-headlines?apiKey=14aeac4e5260419b8b4dd7847b0f6405',
+    searchNewsUrl: 'https://newsapi.org/v2/everything?apiKey=14aeac4e5260419b8b4dd7847b0f6405'
+};
